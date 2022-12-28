44034ae4a06d511d4fb4611a117e6a551cd25ce4
Rinat Alymbetov <votebmyla@gmail.com>
2022-12-28 10:49:56 +0000

    add download feature

--- transfer.sh
+++ transfer.sh
@@ -1,28 +1,82 @@
-#!/bin/curl
+#!/bin/bash
 
 currentVersion="0.0.1"
 
-httpSingleUpload()
-{
-    response=$(curl -A curl --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
+httpSingleUpload(){
+  response=$(curl -A curl --upload-file "$1" "https://transfer.sh/$2") \
+            || { echo "Failure!"; return 1;}
+  printUploadResponse
 }
 
-printUploadResponse()
-{
-fileID=$(echo "$response" | cut -d "/" -f 4)
-  cat <<EOF
-Transfer File URL: $response
-EOF
+
+printUploadResponse(){
+  # cut uploaded file ID
+  local fileID=$(echo "$response" | cut -d "/" -f 4)
+  echo $response
 }
 
-singleUpload()
-{
-  filePath=$(echo "$1" | sed s:"~":"$HOME":g)
-  if ! -f "$filePath" ;then { echo "Error: invalid file path"; return 1;}; fi
-  tempFileName=$(echo "$1" | sed "s/.*\///")
-  echo "Uploading $tempFileName"
-  httpSingleUpload "$filePath $tempFileName"
+singleUpload(){
+  # iterate files to upload
+  for item in "$@";
+  do
+    # replace '~' sign with user's home directory
+    filePath=$(echo "$item" | sed s:"~":"$HOME":g)
+    # check if file exists
+    if ! [ -f "$filePath" ];
+    then
+      echo "Error: invalid file path" 
+      return 1 
+    fi
+    # cut the file name and assign to the tempFileName variable
+    tempFileName=$(echo "$item" | sed "s/.*\///")
+    echo "Uploading $tempFileName"
+    httpSingleUpload $filePath $tempFileName
+  done
 }
 
-singleUpload "$1" || exit 1
-printUploadResponse
+# sample file id for download: ppkkFf
+# sampleID="nVjufu"
+
+singleDownload(){
+  local saveDirectory="${1}"
+  local saveFileName="${3}"
+  local downloadFileID="${2}"
+
+  # echo $1 $2
+  echo "Downloading $saveFileName"
+  curl "https://transfer.sh/$downloadFileID/$saveFileName" \
+        -o "${saveDirectory}/${saveFileName}" \
+        || { echo "Failure!"; return 1;}
+  printDownloadResponse
+  # echo "This is the SINGLEDOWNLOAD function with $1 argument"
+}
+
+printDownloadResponse(){
+  echo "Success!"
+}
+
+
+while getopts "hd:" options
+do
+  case "${options}" in
+    d)
+      # echo "2) $2"
+      # echo "3) $3"
+      # echo "4) $4"
+      singleDownload $2 $3 $4
+      exit 0
+      ;;
+    h)
+      echo "Description: Bash tool to transfer files from the command line.
+Usage:
+  -d  ...
+  -h  Show the help ... 
+  -v  Get the tool version
+Examples:
+  <Write a couple of examples, how to use your tool>"
+      exit 0
+      ;;
+  esac
+done
+
+singleUpload "$@" || exit 1
